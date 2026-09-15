a9734048525b81cad49b00f595dccec56b188c4b
NazihSaba <81354231+nazihsaba@users.noreply.github.com>
2026-09-15 18:17:18 +0000

    Change Env Variables Names

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,6 +6,6 @@ import { createClient } from "@supabase/supabase-js";
  * businesses table allows select and nothing else.
  */
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUB_SUPABASE_URL!,
+  process.env.NEXT_PUB_SUPABASE_ANON_KEY!,
 );
